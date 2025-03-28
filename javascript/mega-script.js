1b38ca4b465acfdf823b85ef561f3fe050d1cc17
Simon Tech <contato.simon.tech@gmail.com>
2025-03-28 15:48:16 -0300

    começo lógica js

---
+++ javascript/mega-script.js
@@ -0,0 +1,12 @@
+const result = document.getElementById('result');
+const areasave = document.getElementById('areasave')
+const n1 = document.getElementById('n1');
+const n2 = document.getElementById('n2');
+const n3 = document.getElementById('n3');
+const n4 = document.getElementById('n4');
+const n5 = document.getElementById('n5');
+const n6 = document.getElementById('n6');
+
+function generateNumbers() {
+    
+}
